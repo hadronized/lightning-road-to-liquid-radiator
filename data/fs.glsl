c62724cc4807b8de81d01864b6147e8df6223d27
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-03-26 22:00:36 +0100

    Hihi.

--- data/fs.glsl
+++ data/fs.glsl
@@ -1,7 +1,32 @@
 #version 130
 
-out vec4 frag;
+precision highp float;
 
+uniform float time;
+uniform vec2 resolution;
+
+vec2 getUV() {
+  vec2 uv = vec2(1. - 2. * gl_FragCoord.x / resolution.x,
+                 1. - 2. * gl_FragCoord.y / resolution.y);
+  uv.x *= (resolution.x / resolution.y);
+  return uv;
+}
+ 
+float rand(vec2 co){
+  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+  
+float plasma(vec2 uv) {
+  return
+    sin(uv.x*6.-time) + sin(uv.y*8.)
+  + sin((uv.x+uv.y)*8.+time)
+  + sin(length(uv)*4.+time)
+  + sin(uv.x*uv.y*8.+time)
+  + sin(distance(uv, vec2(sin(time),cos(time)))*5.+time);
+}
+   
 void main() {
-  frag = vec4(0.5f, 1.f, 0.5f, 1.f);
+  vec2 uv = getUV();
+  float scanline = mod(gl_FragCoord.y, 2.0);
+  gl_FragColor = scanline * vec4(1.-plasma(uv)/3., 0.5-plasma(uv), plasma(uv)*sin(time), 1.);
 }
