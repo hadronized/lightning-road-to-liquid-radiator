0f00bbb1704f1107c3d6bf54d5f85a8b3828636c
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-03-27 19:59:51 +0100

    Plasma!

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ namespace {
   string const FSPATH = "./data/fs.glsl";
 }
 
-string load_source(string path) {
+string load_source(string const &path) {
   ifstream fh(path.c_str());
   string s;
 
@@ -29,9 +29,6 @@ string load_source(string path) {
     throw "file not found: " + path;
   }
 
-  cout << "---------- SHADER SOURCE ----------" << endl;
-  cout << s << endl;
-  cout << "---------- SHADER SOURCE ----------" << endl;
   return s;
 }
 
@@ -92,6 +89,12 @@ GLuint gen_program() {
   return sp;
 }
 
+GLint map_uniform(string const &name, GLuint sp) {
+  GLchar const *n = name.c_str();
+  GLint id = glGetUniformLocation(sp, n);
+  return id;
+}
+
 int main() {
   SDL_Surface *pScreen = 0;
   SDL_Event event;
@@ -103,8 +106,19 @@ int main() {
 
   auto sp = gen_program();
   glUseProgram(sp);
+  auto res = map_uniform("res", sp);
+  auto time = map_uniform("time", sp);
+  if (res != -1) {
+    cout << "res is active" << endl;
+    glUniform2f(res, WIDTH, HEIGHT);
+  }
+  if (time != -1) {
+    cout << "time is active" << endl;
+  }
+  float tf = 0.f;
   while (loop) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUniform1f(time, tf);
     glRectf(-1.f,1.f,1.f,-1.f);
     SDL_GL_SwapBuffers();
 
@@ -115,6 +129,8 @@ int main() {
           break;
       }
     }
+    
+    tf += 0.01f;
   }
 
   glDeleteProgram(sp);
