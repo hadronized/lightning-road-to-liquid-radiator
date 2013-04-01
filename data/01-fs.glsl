f3baf662df10019c7c9e86b45d70203ac8c227a0
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-04-01 21:42:25 +0200

    Colored cube!

--- data/01-fs.glsl
+++ data/01-fs.glsl
@@ -6,9 +6,21 @@ in vec3 pos;
 in vec3 no;
 out vec4 frag;
 
+float plasma() {
+  return
+    + sin((pos.x+pos.y)*4.)
+    + sin(length(pos)*18+time*8.)
+    + sin(pos.z*pos.y*8.)
+    + sin(pos.x*pos.z*8.)
+    + sin(distance(pos, vec3(cos(time)*2., -0.5, sin(time)*2.))*8.)
+    ;
+}
+
 void main() {
   vec3 lpos = vec3(0., -2., 2.8);
-  float d = dot(normalize(lpos - pos), no);
-  frag = d * vec4(1.f);
+  vec3 ldir = normalize(lpos - pos);
+  float d = dot(ldir, no);
+  float pv = plasma();
+  frag = vec4(1. - pv/3, 0.5 - pv, pv*sin(time), 1.) * d;
 }
 
