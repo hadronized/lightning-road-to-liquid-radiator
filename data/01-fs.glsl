83929cc3d56c57f213a3023acf6e5348fc3ddc9f
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-04-04 11:32:43 +0200

    We don’t use the plasma for the cube now.

--- data/01-fs.glsl
+++ data/01-fs.glsl
@@ -19,25 +19,12 @@ float plasma() {
 void main() {
   vec3 lpos = vec3(0., -1., 2.8);
   vec3 ldir = normalize(lpos - pos);
-  float d = dot(ldir, no);
+  float d = max(0., dot(ldir, no));
   vec3 r = reflect(no, normalize(pos - vec3(0., 0., 2.)));
-  float s = pow(max(0., dot(ldir,r)), 3.);
+  float s = pow(max(0., dot(-ldir,r)), 2.);
   float pv = plasma();
   float scanline = mod(gl_FragCoord.y, 2.);
-  if (time < 10.) { /* phong */
-    frag = vec4(1.) * (d+s);
-  } else if (time < 14.) { /* blue line swap */
-      if (pos.y < (time-12.)) {
-        frag = vec4(1. - pv/3, 0.5 - pv, pv*sin(time), 1.) * d;
-      } else if (pos.y >= (time-12.) && pos.y <= (time+0.01-12.)) {
-        frag = vec4(0., 0.5, 0.33, 1.);
-        frag *= scanline;
-      } else {
-        frag = vec4(1.) * (d+s);
-      }
-  } else {
-    frag = vec4(1. - pv/3, 0.5 - pv, pv*sin(time), 1.) * (d+s);
-    frag *= scanline;
-  }
+
+  frag = vec4(1.) * (d+s);
 }
 
