52a7ba4d74559ddc28e38efe61ea0119291db943
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-04-18 21:26:31 +0200

    nts.

--- data/01-pp-fs.glsl
+++ data/01-pp-fs.glsl
@@ -7,7 +7,7 @@ uniform vec2 res;
 uniform float time;
 
 vec2 uv_tex() {
-  float f = max(1., min(100., 100. - (time-12.80)*80.));
+  float f = max(1., min(70., 70. - (time-12.80)*80.));
   vec2 uv = vec2(floor(gl_FragCoord.x / f) / (res.x/f), floor(gl_FragCoord.y / f) / (res.y/f));
   
   return uv;
