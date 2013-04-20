caed7b8c7d2488be6ce96e5c6d4f49c3826b357b
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-04-21 01:32:53 +0200

    Added a vignette effect to the tunnel part.

--- data/02-fs.glsl
+++ data/02-fs.glsl
@@ -31,9 +31,10 @@ void main() {
   float pip = (ray.y < 0. ? -1. : 1.);
   vec2 lookup = vec2(mod(hit*0.01, 1.), acos(normalize(pip*ray.xy).x) / PI);
   float atten = max(1., 0.2 * hit);
+  float v = max(0.1, min(1., 1.5 - pow(length(vec2(uv.x, uv.y * (res.x/res.y))), 2.)));
 
   float f = tex(lookup+vec2(0., time/5.))/atten;
   frag = vec4(f/2., f*sin(time)/2., 0., 1.) * mod(gl_FragCoord.y, 2.);
-  //frag *= distance(get_uv(), vec2(0.0));
+  frag *= v;
 }
 
