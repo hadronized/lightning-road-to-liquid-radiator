63e013d9488d2ae04b34d87f00df5ccd296b9f34
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-05-04 01:25:48 +0200

    Nice thunders!

--- data/thun-blur-fs.glsl
+++ data/thun-blur-fs.glsl
@@ -12,21 +12,21 @@ vec2 get_uv() {
 void main() {
   vec2 uv = get_uv();
   float step = res.z*1.5;
-  frag = texture2D(offtex, vec2(uv.x-step*3., uv.y)) * 0.05
+  frag = texture2D(offtex, vec2(uv.x-step*6., uv.y)) * 0.05
        + texture2D(offtex, vec2(uv.x-step*2., uv.y)) * 0.10
-       + texture2D(offtex, vec2(uv.x-step, uv.y)) * 0.40
+       + texture2D(offtex, vec2(uv.x-step, uv.y)) * 0.25
        + texture2D(offtex, vec2(uv.x, uv.y)) * 0.5
-       + texture2D(offtex, vec2(uv.x+step, uv.y)) * 0.40
+       + texture2D(offtex, vec2(uv.x+step, uv.y)) * 0.25
        + texture2D(offtex, vec2(uv.x+step*2., uv.y)) * 0.10
-       + texture2D(offtex, vec2(uv.x+step*3., uv.y)) * 0.05
+       + texture2D(offtex, vec2(uv.x+step*6., uv.y)) * 0.05
        ;
-  frag += texture2D(offtex, vec2(uv.x, uv.y-step*3.)) * 0.05
+  frag += texture2D(offtex, vec2(uv.x, uv.y-step*6.)) * 0.05
         + texture2D(offtex, vec2(uv.x, uv.y-step*2.)) * 0.10
-        + texture2D(offtex, vec2(uv.x, uv.y-step)) * 0.40
+        + texture2D(offtex, vec2(uv.x, uv.y-step)) * 0.25
         + texture2D(offtex, vec2(uv.x, uv.y)) * 0.5
-        + texture2D(offtex, vec2(uv.x, uv.y+step)) * 0.40
+        + texture2D(offtex, vec2(uv.x, uv.y+step)) * 0.25
         + texture2D(offtex, vec2(uv.x, uv.y+step*2.)) * 0.10
-        + texture2D(offtex, vec2(uv.x, uv.y+step*3.)) * 0.05
+        + texture2D(offtex, vec2(uv.x, uv.y+step*6.)) * 0.05
         ;
   frag *= 0.5;
 }
