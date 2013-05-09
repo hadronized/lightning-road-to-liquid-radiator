dd716e3651dcd688f05a465686e17be87b93df54
Dimitri 'skypers' Sabadie <dimitri.sabadie@gmail.com>
2013-05-09 10:57:15 +0200

    Akunamatata!

--- src/mod3.cpp
+++ src/mod3.cpp
@@ -92,7 +92,7 @@ void mod3_c::render(float time) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   _writer.start_draw();
   if (time < 150.9f) {
-    /* :: greetings */
+    /* greetings */
     _writer.draw_string(":: greetings ::", -0.45f, std::min(0.2f, t-1.f), h+0.03f);
     _writer.draw_string("xtrium, XT95, rez, latortue, sam", std::min(-0.6f, t-3.f), 0.1f, h);
     _writer.draw_string("zerkman, maracuja, piark, hellflip", std::max(-0.64f, -t+2.f), 0.f, h);
@@ -100,6 +100,7 @@ void mod3_c::render(float time) {
     _writer.draw_string("all #demofr sceners! ...", -0.45f, std::max(-0.2f, -t+3.f), h);
     _writer.draw_string("... and everyone at Outline!", -0.95f, std::max(-0.4f, -4.f*(t-5.f)), h+0.04f);
   } else {
+    /* end of the intro */
     _writer.draw_string("Ligthning Road To Liquid Radiator", -0.9f, 0.f, h+0.02f);
     _writer.draw_string("Outline 2013\n"
                         "May 9th-12th\n"
